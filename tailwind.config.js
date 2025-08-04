6efb5a11009088f278b3f54e4de6694ce1a82143
tanishka0324 <tanishka03242008@gmail.com>
2025-08-05 01:26:08 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,3 +3,10 @@ module.exports = {
   theme: { extend: {} },
   plugins: [],
 };
+
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+}
